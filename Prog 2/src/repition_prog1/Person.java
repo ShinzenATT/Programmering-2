9e03f278acc792319f0f0b630c0c2bd01481ef3e
ShinzenATT <alen.mukaca@gmail.com>
2019-08-27 12:42:22 +0200

    Repitition Uppgift 6

--- Prog 2/src/repition_prog1/Person.java
+++ Prog 2/src/repition_prog1/Person.java
@@ -1,12 +1,39 @@
 package repition_prog1;
 
 public class Person {
-	static String name = "";
-	static int age = 0;
+	String name = "";
+	int age = 0;
 	
 	public Person(String naming, int aging){
 		name = naming;
 		age = aging;
 		
 	}
+	
+	public int age() {
+		return age;
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public void oldest(Person a, Person b) {
+		if(a.age() > b.age()) {
+			System.out.println(a.name() + " is the oldest.");
+		}
+		else if(a.age() < b.age()) {
+			System.out.println(b.name() + " is the oldest.");
+		}
+		else {
+			System.out.println("They are the same age.");
+		}
+		return;
+	}
+	
+	public void oldest(Person b) {
+		oldest(new Person(name, age), b);
+		return;
+	}
+	
 }
---
+++ Prog 2/src/repition_prog1/Upg6.java
@@ -0,0 +1,9 @@
+package repition_prog1;
+
+public class Upg6 {
+	public static void main(String[] args) {
+		Person dude = new Person("Fredrik", 20);
+		Person gril = new Person("Layla", 16);
+		dude.oldest(gril);
+	}
+}
