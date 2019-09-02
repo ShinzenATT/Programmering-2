fda907433e92fd9e2c647539b670d2840da10c17
ShinzenATT <alen.mukaca@gmail.com>
2019-09-02 10:34:35 +0200

    Repitition del 2 Uppgift 1 Omgjord

--- Prog 2/src/repitition2/Upg1.java
+++ Prog 2/src/repitition2/Upg1.java
@@ -1,26 +1,13 @@
-package repitition2;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+package repitition2;;
 
 public class Upg1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ArrayList<Integer> term = new ArrayList<Integer>();
-        System.out.println("Skriv tal som ska summeras och avsluta med 0.");
-        while (true){
-            int temp = input.nextInt();
-            if(temp == 0){
-                break;
-            }
-            else{
-                term.add(temp);
-            }
-        }
+
+    public static int sum (int... term){
+
         int sum = 0;
-        for (int i = 0; i < term.size() ; i++) {
-            sum+= term.get(i);
+        for (int i = 0; i < term.length ; i++) {
+            sum+= term[i];
         }
-        System.out.println("Summa: " + sum);
+        return sum;
     }
 }
