ef3279704b5dabc1fa807bf9a030dd4a9d443943
ShinzenATT <alen.mukaca@gmail.com>
2019-09-02 10:29:34 +0200

    Repitition del 2 Uppgift 3

---
+++ Prog 2/src/repitition2/Upg3.java
@@ -0,0 +1,17 @@
+package repitition2;
+
+import java.util.Scanner;
+
+public class Upg3 {
+    public static Object[] object2array (Object... args){
+        /*
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i].toString());
+        } */
+        return args;
+    }
+
+    /* public static void main(String[] args) {
+        object2array(new Scanner(System.in), 10, "Nope");
+    } */
+}
