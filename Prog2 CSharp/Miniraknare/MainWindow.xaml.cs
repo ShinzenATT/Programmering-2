deec7fffbae0546cf2341b1206c2f6bc2bb1cbf9
Alen M <alen.mukaca@gmail.com>
2019-10-11 13:01:35 +0200

    Miniräknare: UI + genrisk siffra eventhandeler

--- Prog2 CSharp/Miniraknare/MainWindow.xaml.cs
+++ Prog2 CSharp/Miniraknare/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace Miniraknare
         {
             InitializeComponent();
         }
+
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Applies/Removes a placeholder from text box when focus changes and there's no user written value.
+
+        private void NumberBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (NumberBox.Text.Equals("Write here..."))
+            {
+                NumberBox.Text = "";
+                NumberBox.Foreground = new SolidColorBrush(Color.FromRgb(57,57,57));
+            }
+        }
+
+        private void NumberBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(NumberBox.Text == "")
+            {
+                NumberBox.Foreground = new SolidColorBrush(Color.FromRgb(105,105,105));
+                NumberBox.Text = "Write here...";
+            }
+            
+        }
+
+        private void Numpad_Input(object sender, RoutedEventArgs e)
+        {
+            String a = sender.ToString();
+            MessageBox.Show(a.Substring(a.Length - 2));
+        }
+    }
+}
+
+namespace Miniraknare
+{
+    public class QueryHistory
+    {
+
     }
 }
