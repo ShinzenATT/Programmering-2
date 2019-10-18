78d345b7280933204300889489b9fcdc661edc63
Alen M <alen.mukaca@gmail.com>
2019-10-18 14:00:15 +0200

    Påbörjade abstrakt klass

---
+++ Prog2 CSharp/Miniraknare/Calc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniraknare
+{
+    public abstract class Calc<T>
+    {
+        private List<object> QueryHistory = new List<object>();
+        protected double Add(double number1, double number2)
+        {
+            return (number1 + number2);
+        }
+
+        protected double Minus(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        protected double Divide(double numerator, double denominator)
+        {
+            return numerator / denominator;
+        }
+       
+        protected double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        protected void RemovePreviousObjectExe()
+        {
+            QueryHistory.RemoveAt(QueryHistory.Count - 1);
+        }
+
+        public abstract void RemovePreviousObject();
+
+        protected abstract T Add(T term1, T term2);
+        protected abstract T Minus(T term1, T term2);
+        protected abstract T Divide(T numerator, T denumerator);
+        protected abstract T Multiply(T factor1, T factor2);
+
+    }
+}
--- Prog2 CSharp/Miniraknare/MainWindow.xaml.cs
+++ Prog2 CSharp/Miniraknare/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Miniraknare
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly string[] legalCharacters = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "%"};
+        static CalcExecution calc = new CalcExecution();
         public MainWindow()
         {
             InitializeComponent();
@@ -34,7 +36,7 @@ namespace Miniraknare
 
         private void NumberBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (NumberBox.Text.Equals("Write here..."))
+            if (NumberBox.Text.Equals("0"))
             {
                 NumberBox.Text = "";
                 NumberBox.Foreground = new SolidColorBrush(Color.FromRgb(57,57,57));
@@ -46,22 +48,55 @@ namespace Miniraknare
             if(NumberBox.Text == "")
             {
                 NumberBox.Foreground = new SolidColorBrush(Color.FromRgb(105,105,105));
-                NumberBox.Text = "Write here...";
+                NumberBox.Text = "0";
             }
             
         }
 
+        // Generic eventhandeler to input numbers to text box
         private void Numpad_Input(object sender, RoutedEventArgs e)
         {
-            String a = sender.ToString();
-            MessageBox.Show(a.Substring(a.Length - 2));
+            string tempNumber = sender.ToString();
+            if (!(NumberBox.IsFocused))
+            {
+                NumberBox_GotFocus(null, null);
+            }
+            NumberBox.Text += tempNumber.Substring(tempNumber.Length - 1);
+            return;
+        }
+
+        //Button eventhandeler to clear the current entry
+        private void CleanEntry_Click(object sender, RoutedEventArgs e)
+        {
+            NumberBox.Text = "";
+            NumberBox_LostFocus(null, null);
+        }
+
+        // Eventhandeller to check if any unlegal characters are inputed into text box
+        private void NumberBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            for (int i = NumberBox.Text.Length - 1; i >= 0; i--)
+            {
+                string LastByte = NumberBox.Text.Substring(i, 1);
+                if (NumberBox.Text.Length != 0 && !(legalCharacters.Contains(LastByte)))
+                {
+                    NumberBox.Text = NumberBox.Text.Replace(LastByte, "");
+                }
+            }
+            }
+            
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            NumberBox.Focus();
         }
     }
 }
 
 namespace Miniraknare
 {
-    public class QueryHistory
+    public class CalcExecution : Calc
     {
 
     }
