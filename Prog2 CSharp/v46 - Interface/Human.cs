9dedb7d7390cf8206b10b538dba37c55fc0ca5a8
ShinzenATT <alen.mukaca@gmail.com>
2019-11-11 10:57:57 +0100

    Interface Lab v46

---
+++ Prog2 CSharp/v46 - Interface/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v46___Interface
+{
+    interface IHuman
+    {
+        string interest { get; }
+        string name { get;}
+        bool willToLive { get; set; }
+
+        string InterestAction();
+
+        string Introduction();
+
+        string Philosophy();
+
+        string WakeUp();
+
+        string BedTime();
+
+        string Work();
+
+    }
+}
---
+++ Prog2 CSharp/v46 - Interface/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v46___Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time for a story. Click any key to continue");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            IHuman[] h = { new Humans.Bob(), new Humans.Bob() };
+
+            foreach (IHuman human in h)
+            {
+                string[] story = { human.Introduction(), human.WakeUp(), human.Work(), human.Philosophy(), human.InterestAction(), human.BedTime() };
+                foreach (string piece in story)
+                {
+                    Console.WriteLine(piece);
+                    Console.ReadKey();
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
