64896d358cc1a5c808659890d4a5f3e62683924e
ShinzenATT <42674003+ShinzenATT@users.noreply.github.com>
2019-11-22 14:03:00 +0100

    Added interface & contexts

---
+++ Prog2 CSharp/v47 - Solid/ICalculation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v47___Solid
+{
+    interface ICalculation<T>
+    {
+        char Sign { get; }
+
+        T DoOperation(T num1, T num2);
+
+        bool ContainsSign(string str);
+        bool ContainsSign(char letter);
+    }
+}
