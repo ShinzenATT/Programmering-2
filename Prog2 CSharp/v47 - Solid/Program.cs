0b4bafbf08d303727eb8cd60cb3828d890607a92
ShinzenATT <42674003+ShinzenATT@users.noreply.github.com>
2019-11-22 14:03:28 +0100

    Started working on demo

--- Prog2 CSharp/v47 - Solid/Program.cs
+++ Prog2 CSharp/v47 - Solid/Program.cs
@@ -8,8 +8,25 @@ namespace v47___Solid
 {
     class Program
     {
+        static readonly ICalculation<double>[] Arithmatics = { new Add(), new Subtract() };
+
+        static int i = 0;
+
         static void Main(string[] args)
         {
+            string input = "";
+            do
+            {
+                Console.WriteLine("Write an expression");
+                input = Console.ReadLine();
+                string[] expression = input.Split(' ');
+
+
+                for (i = 0; i < Arithmatics.Length && expression.Length > 1; i++)
+                {
+
+                }
+            } while (!(input.ToLower().Contains("exit")));
         }
     }
 }
