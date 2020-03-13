939be5df84b6584401b5259e28738ead64dc5b82
Alen.M <42674003+ShinzenATT@users.noreply.github.com>
2020-03-13 13:06:09 +0100

    MVC Pattern

---
+++ Prog2 CSharp/MVC Pattern/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Pattern
+{
+    public class StudentController
+    {
+        private Student model;
+        private StudentView view;
+
+        public string StudentName { 
+            get { return model.Name; }
+            set { model.Name = value; }
+        }
+
+        public string StudentRollNo
+        {
+            get { return model.RollNo; }
+            set { model.RollNo = value; }
+        }
+
+        public StudentController(Student model, StudentView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void UpdateView()
+        {
+            view.PrintStudentDetails(StudentName, StudentRollNo);
+        }
+    }
+}
---
+++ Prog2 CSharp/MVC Pattern/View.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Pattern
+{
+    public class StudentView
+    {
+        public void PrintStudentDetails(string studentName, string studentRollNo)
+        {
+            Console.Clear();
+            Console.WriteLine("Student:");
+            Console.WriteLine("Name: " + studentName);
+            Console.WriteLine("Roll No: " + studentRollNo);
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void PrintStudentDetails(Student student)
+        {
+            PrintStudentDetails(student.Name, student.RollNo);
+        }
+    }
+}
